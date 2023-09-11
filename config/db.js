6ed70dbf2f38b9152f52fb7b4b50818d7c5a38d7
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-09-11 16:08:57 +0530

    connection string for deployment on rugged

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const connectDB=async ()=>
 {
     try
     {
+        const connString=process.env.MONGO_URI || "mongodb+srv://poorvkumar14:Ironman3000@motorq-sde.llahct9.mongodb.net/";
         const conn=await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.red.bold.underline);
